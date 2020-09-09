a1459184cf66fe93e9cafcc7c3969f3c164aceeb
John Cross <john.william.cross@gmail.com>
2020-09-09 14:27:54 -0700

    add more content to question.html, add a link

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(".form-control").on("input", function () {
    // code below targets the id "overlay-sucess"; for toggleClass,
    // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
    $(".search-results").toggleClass("d-flex d-none");
+   $(".ask-new-question").toggleClass("d-none");
+});
+
+$(".submit-question-button").onclick(function () {
+   $;
 });
