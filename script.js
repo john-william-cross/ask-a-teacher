25951ad3e5e4a54f94278cd5451f3aa7c5cca6b8
John Cross <john.william.cross@gmail.com>
2020-09-13 12:33:39 -0700

    continue styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(".form-control").on("input", function () {
    // code below targets the id "overlay-sucess"; for toggleClass,
    // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
-   $(".search-results").toggleClass("d-flex d-none");
+   $(".question").toggleClass("d-flex d-none");
    $(".ask-new-question").toggleClass("d-flex d-none");
 });
 
