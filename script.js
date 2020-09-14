8ae5aeb05f0afd06b47278f55cf4d32effef3284
John Cross <john.william.cross@gmail.com>
2020-09-14 16:39:41 -0700

    fix that dang underline

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(".form-control").on("input", function () {
    // code below targets the id "overlay-sucess"; for toggleClass,
    // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
-   $(".question").toggleClass("d-flex d-none");
+   $(".question").toggleClass("d-none");
    $(".ask-new-question").toggleClass("d-flex d-none");
 });
 
