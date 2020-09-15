a6ff3c1416f57ef8c433e61632f9e2bcb73611f0
John Cross <john.william.cross@gmail.com>
2020-09-15 09:58:27 -0700

    add more consistency to design

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(".form-control").on("input", function () {
    // code below targets the id "overlay-sucess"; for toggleClass,
    // it means "if d-flex is on, turn it off; if d-none is on, turn it off"
    $(".question").toggleClass("d-none");
-   $(".ask-new-question").toggleClass("d-flex d-none");
+   $(".submit-new-question").toggleClass("d-none");
 });
 
 $(".answer-a-question").click(function () {
