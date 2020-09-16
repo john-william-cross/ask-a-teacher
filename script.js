1bce5b9104d71f9ad75ab2553ade3633457ceb4b
John Cross <john.william.cross@gmail.com>
2020-09-16 12:59:40 -0700

    added jquery char count using Strings

--- script.js
+++ script.js
@@ -15,31 +15,6 @@ $(".sign-up-prompt").click(function () {
    $(".email-and-create-password").toggleClass("d-none");
 });
 
-let questionInputCharCount = 0;
-
-// $("#question-input").keydown(function (e) {
-//    const key = e.which;
-//    console.log("key inputted: ", key);
-//    if (key === 8) {
-//       console.log("the user has pressed backspace!");
-//       questionInputCharCount--;
-//       if (questionInputCharCount < 0) {
-//          console.log("You have entered negative territory!");
-//          questionInputCharCount = 0;
-//       }
-//    } else if (key === 16) {
-//       console.log("Shift was pressed");
-//    } else if (key === 18) {
-//       console.log("Option was pressed");
-//    } else {
-//       console.log("the user pressed any other key");
-//       questionInputCharCount++;
-//    }
-
-//    console.log("total inputted chars: ", questionInputCharCount);
-//    $("#question-input-char-count").html(questionInputCharCount);
-// });
-
 $(`#question-input`).keyup(function (e) {
    console.log(`Event: `, e);
 
@@ -56,6 +31,26 @@ $(`#question-input`).keyup(function (e) {
    $(`#question-input-char-count`).html(textLength);
 
    if (text.length > 240) {
+      console.log(`You've entered more than 500 characters`);
+   }
+});
+
+$(`#answer-input`).keyup(function (e) {
+   console.log(`Event: `, e);
+
+   // get the text from the text area
+   const text = e.target.value;
+   const username = `John`;
+   console.log(`${username} inputted: ${text}`);
+
+   // check the length of the text
+   const textLength = text.length;
+   console.log(`Total inputted chars: ${textLength}`);
+
+   // update the character counter on the page
+   $(`#answer-input-char-count`).html(textLength);
+
+   if (text.length > 500) {
       console.log(`You've entered more than 500 characters`);
    }
 });
