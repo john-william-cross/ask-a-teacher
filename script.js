24f0b3423cc4df869586b2287906b3fc6765b357
John Cross <john.william.cross@gmail.com>
2020-09-16 12:26:06 -0700

    use Strings for input question field char count

--- script.js
+++ script.js
@@ -17,25 +17,45 @@ $(".sign-up-prompt").click(function () {
 
 let questionInputCharCount = 0;
 
-$("#question-input").keydown(function (e) {
-   const key = e.which;
-   console.log("key inputted: ", key);
-   if (key === 8) {
-      console.log("the user has pressed backspace!");
-      questionInputCharCount--;
-      if (questionInputCharCount < 0) {
-         console.log("You have entered negative territory!");
-         questionInputCharCount = 0;
-      }
-   } else if (key === 16) {
-      console.log("Shift was pressed");
-   } else if (key === 18) {
-      console.log("Option was pressed");
-   } else {
-      console.log("the user pressed any other key");
-      questionInputCharCount++;
-   }
+// $("#question-input").keydown(function (e) {
+//    const key = e.which;
+//    console.log("key inputted: ", key);
+//    if (key === 8) {
+//       console.log("the user has pressed backspace!");
+//       questionInputCharCount--;
+//       if (questionInputCharCount < 0) {
+//          console.log("You have entered negative territory!");
+//          questionInputCharCount = 0;
+//       }
+//    } else if (key === 16) {
+//       console.log("Shift was pressed");
+//    } else if (key === 18) {
+//       console.log("Option was pressed");
+//    } else {
+//       console.log("the user pressed any other key");
+//       questionInputCharCount++;
+//    }
 
-   console.log("total inputted chars: ", questionInputCharCount);
-   $("#question-input-char-count").html(questionInputCharCount);
+//    console.log("total inputted chars: ", questionInputCharCount);
+//    $("#question-input-char-count").html(questionInputCharCount);
+// });
+
+$(`#question-input`).keyup(function (e) {
+   console.log(`Event: `, e);
+
+   // get the text from the text area
+   const text = e.target.value;
+   const username = `John`;
+   console.log(`${username} inputted: ${text}`);
+
+   // check the length of the text
+   const textLength = text.length;
+   console.log(`Total inputted chars: ${textLength}`);
+
+   // update the character counter on the page
+   $(`#question-input-char-count`).html(textLength);
+
+   if (text.length > 240) {
+      console.log(`You've entered more than 500 characters`);
+   }
 });
