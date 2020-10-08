925ea14bbcea718dd95a1107ffbee5270990a084
John Cross <john.william.cross@gmail.com>
2020-10-08 07:45:29 -0700

    add regex for email validation for teacher sign up (require ccsd email address)

--- getEmailError.js
+++ getEmailError.js
@@ -1,6 +1,12 @@
 function getEmailError(email) {
+   const emailPatternCcsd = /@nv.ccsd.net$/;
+
+   const isValidEmail = emailPatternCcsd.test(email);
+
    if (email === ``) {
       return `Please enter your email address.`;
+   } else if (isValidEmail === false) {
+      return `Please enter a valid email address.`;
    } else {
       return ``;
    }
